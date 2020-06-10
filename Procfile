ad2e8f2a022c03d78ff2130d0d5e0570ce4aa998
dmdekf <dmdekf@gmail.com>
2020-06-10 21:04:57 +0900

    edit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn djangovuepjt.wsgi --log-file -
+web: gunicorn articleBack.wsgi --log-file -
--- articleBack/settings.py
+++ articleBack/settings.py
@@ -63,7 +63,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
     'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -73,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'articleBack.urls'
